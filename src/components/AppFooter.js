e4d66c115f83f29db2ab7b38d0c6eb342d8dd836
jcano <jcano@thedataage.com>
2022-02-21 14:12:51 -0600

    se defendio

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -11,9 +11,9 @@ const AppFooter = () => {
         <span className="ms-1">&copy; 2021 Consulting Group.</span>
       </div>
       <div className="ms-auto">
-        <span className="me-1">by</span>
+        <span className="me-1"> </span>
         <a href="/" target="_blank" rel="noopener noreferrer">
-          Jerry Cano
+          Prototipo Demostrativo
         </a>
       </div>
     </CFooter>
